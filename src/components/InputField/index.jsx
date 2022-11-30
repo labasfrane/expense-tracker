a974f27969276123e5614f1828e6fa3739523a76
Frane <labasfrane@gmail.com>
2022-11-30 17:55:18 +0100

    Added colors constants

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,6 +1,7 @@
 import { cloneElement } from "react";
 import { useFormContext } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { COLOR_DANGER, COLOR_PRIMARY } from "constants/colors.constants";
 
 import "./index.scss";
 
@@ -13,8 +14,11 @@ const InputField = ({ label, icon, children, formControl = null }) => {
     <div className="input-field">
       <label className="input-field__label">{label}</label>
       <div className="input-field__input">
-        <FontAwesomeIcon icon={icon} />
         {content}
+        <FontAwesomeIcon
+          icon={icon}
+          color={errorMessage ? COLOR_DANGER : COLOR_PRIMARY}
+        />
       </div>
       <span className="input-field__error">{errorMessage}</span>
     </div>
---
+++ src/constants/colors.constants.jsx
@@ -0,0 +1,8 @@
+export const COLOR_PRIMARY = "#2f74c8";
+export const COLOR_SECONDARY = "#2169a3";
+export const COLOR_TERTIARY = "#408fce";
+export const COLOR_GREY = "#cbcbcb";
+export const COLOR_LIGHTGREY = "#e6e6e4";
+export const COLOR_SUCCESS = " white";
+export const COLOR_WHITE = " green";
+export const COLOR_DANGER = " red";
